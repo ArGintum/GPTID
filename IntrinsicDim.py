8c8759ef94c8769e2f40f3e507f270e4c948a562
Eduard Tulchinskiy <81860007+ArGintum@users.noreply.github.com>
2025-03-15 03:22:29 +0300

    Added support for precomputed distance input

--- IntrinsicDim.py
+++ IntrinsicDim.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.spatial.distance import cdist
 from threading import Thread
 
-MINIMAL_CLOUD = 47
+MINIMAL_CLOUD = 80
 
 def prim_tree(adj_matrix, alpha=1.0):
     infty = np.max(adj_matrix) + 10
@@ -29,27 +29,30 @@ def process_string(sss):
 
 class PHD():
     def __init__(self, alpha=1.0, metric='euclidean', n_reruns=3, n_points=7, n_points_min=3):
-'''
-Initializes the instance of PH-dim computer
-Parameters:
-	1) alpha --- real-valued parameter Alpha for computing PH-dim (see the reference paper). Alpha should be chosen lower than 
-the ground-truth Intrinsic Dimensionality; however, Alpha=1.0 works just fine for our kind of data.
-	2) metric --- String or Callable, distance function for the metric space (see documentation for Scipy.cdist)
-	3) n_reruns --- Number of restarts of whole calculations (each restart is made in a separate thread)
-	4) n_points --- Number of subsamples to be drawn at each subsample
-	5) n_points_min --- Number of subsamples to be drawn at larger subsamples (more than half of the point cloud)
-'''
+        '''
+        Initializes the instance of PH-dim computer
+        Parameters:
+        1) alpha --- real-valued parameter Alpha for computing PH-dim (see the reference paper). Alpha should be chosen lower than the ground-truth Intrinsic Dimensionality; however, Alpha=1.0 works just fine for our kind of data.
+        2) metric --- String or Callable, distance function for the metric space (see documentation for Scipy.cdist)
+        3) n_reruns --- Number of restarts of whole calculations (each restart is made in a separate thread)
+        4) n_points --- Number of subsamples to be drawn at each subsample
+        5) n_points_min --- Number of subsamples to be drawn at larger subsamples (more than half of the point cloud)
+        '''
         self.alpha = alpha
         self.n_reruns = n_reruns
         self.n_points = n_points
         self.n_points_min = n_points_min
         self.metric = metric
         self.is_fitted_ = False
+        self.distance_matrix = False
 
     def _sample_W(self, W, nSamples):
         n = W.shape[0]
         random_indices = np.random.choice(n, size=nSamples, replace=False)
-        return W[random_indices]
+        if self.distance_matrix:
+            return W[random_indices][:, random_indices]
+        else:
+            return W[random_indices]
 
     def _calc_ph_dim_single(self, W, test_n, outp, thread_id):
         lengths = []
@@ -62,7 +65,10 @@ the ground-truth Intrinsic Dimensionality; however, Alpha=1.0 works just fine fo
             reruns = np.ones(restarts)
             for i in range(restarts):
                 tmp = self._sample_W(W, n)
-                reruns[i] = prim_tree(cdist(tmp, tmp, metric=self.metric), self.alpha)
+                if self.distance_matrix:
+                    reruns[i] = prim_tree(tmp, self.alpha)
+                else:
+                    reruns[i] = prim_tree(cdist(tmp, tmp, metric=self.metric), self.alpha)
 
             lengths.append(np.median(reruns))
         lengths = np.array(lengths)
@@ -72,16 +78,18 @@ the ground-truth Intrinsic Dimensionality; however, Alpha=1.0 works just fine fo
         N = len(x)   
         outp[thread_id] = (N * (x * y).sum() - x.sum() * y.sum()) / (N * (x ** 2).sum() - x.sum() ** 2)
         
-    def fit_transform(self, X, y=None, min_points=50, max_points=512, point_jump=40):
-'''
-Computing the PH-dim 
-Parameters:
-	1) X --- point cloud of shape (n_points, n_features), 
-	2) y --- fictional parameter to fit with Sklearn interface
-	3) min_points --- size of minimal subsample to be drawn
-	4) max_points --- size of maximal subsample to be drawn
-	5) point_jump --- step between subsamples
-'''
+    def fit_transform(self, X, y=None, min_points=50, max_points=512, point_jump=40, dist=False):
+        '''
+        Computing the PH-dim 
+        Parameters:
+        1) X --- point cloud of shape (n_points, n_features), or precomputed distance matrix (n_points, n_points) if parameter dist set to 'True'
+        2) y --- fictional parameter to fit with Sklearn interface
+        3) min_points --- size of minimal subsample to be drawn
+        4) max_points --- size of maximal subsample to be drawn
+        5) point_jump --- step between subsamples
+        6) dist --- bool value whether X is a precomputed distance matrix
+        '''
+        self.distance_matrix = dist
         ms = np.zeros(self.n_reruns)
         test_n = range(min_points, max_points, point_jump)
         threads = []
@@ -95,6 +103,3 @@ Parameters:
 
         m = np.mean(ms)
         return 1 / (1 - m)
-        
-
-        
